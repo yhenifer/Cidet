5a584fae2fd485661132ad8c3bb58b782a42c320
yhenifer Pulido Rugeles <97737141+yhenifer@users.noreply.github.com>
2023-05-17 18:17:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function remplazarA() {
+  let lateralA = document.querySelector("#foto")
+  lateralA.setAttribute("src", "https://photos6.spartoo.es/photos/207/20724687/20724687_1200_A.jpg");
+}
+  let botonA = document.querySelector("#a");
+  botonA.addEventListener("click", lateralA);
+
+  function remplazarB() {
+  let lateralB = document.querySelector("#foto")
+  lateralB.setAttribute("src", "https://photos6.spartoo.es/photos/207/20724687/20724687_1200_B.jpg");
+}
+  let botonB = document.querySelector("#b");
+  botonB.addEventListener("click", lateralB);  
+
+  function remplazarC() {
+  let lateralC = document.querySelector("#foto")
+  lateralC.setAttribute("src", "https://photos6.spartoo.es/photos/207/20724687/20724687_1200_C.jpg");
+}
+  let botonC = document.querySelector("#c");
+  botonC.addEventListener("click", lateralC);  
+
+  function remplazarD() {
+  let lateralD = document.querySelector("#foto")
+  lateralD.setAttribute("src", "https://photos6.spartoo.es/photos/207/20724687/20724687_1200_D.jpg");
+}
+  let botonD = document.querySelector("#d");
+  botonD.addEventListener("click", lateralD);  
+
